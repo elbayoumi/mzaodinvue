d7b963ccc78ac241441d604ade8b3cfc234c2f00
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-02-02 09:24:47 +0200

    website

---
+++ app/Http/Controllers/WebSiteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WebSite;
+use Illuminate\Http\Request;
+
+class WebSiteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WebSite $webSite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WebSite $webSite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, WebSite $webSite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WebSite $webSite)
+    {
+        //
+    }
+}
