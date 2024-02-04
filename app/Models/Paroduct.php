374befbe3b73ca8b01b8f98841a826c93e930a72
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-02-04 22:30:04 +0200

    v

--- app/Models/Paroduct.php
+++ app/Models/Paroduct.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Paroduct extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
 }
--- app/Models/ParoductImage.php
+++ app/Models/ParoductImage.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ParoductImage extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
 }
