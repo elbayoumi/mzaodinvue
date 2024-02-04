79d4f45fdeb56f5f299a6cff116f4c9cdf99b683
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-02-04 22:55:06 +0200

    v

--- database/migrations/2024_02_04_205427_create_image_products_table.php
+++ database/migrations/2024_02_04_205427_create_image_products_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('image_products', function (Blueprint $table) {
             $table->id();
-            
             $table->tinyText('src');
 
             $table->tinyText('alt');
