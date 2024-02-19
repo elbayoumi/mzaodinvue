4510ecfe6f252be2a38f5aa9b8c6733fe1bc5901
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-02-20 00:12:54 +0200

    img

--- app/Models/ImageProduct.php
+++ app/Models/ImageProduct.php
@@ -4,16 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class ImageProduct extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    protected $appends = ['image_path'];
+
     function product(){
         return  $this->belongsTo(Product::class,'product_id');
     }
     public function getImgAttribute($value)
     {
         return asset('storage/'.$value);
+    }
+    public function setImgAttribute($value)
+    {
+        if (!empty($this->img)) {
+            Storage::disk('public')->delete($this->img);
+        }
+
+        $this->attributes['img'] = $value;
     }
 }
