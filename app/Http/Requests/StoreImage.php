10b9cec6dd3371ad14e3a550010aa70aba2105e0
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-02-20 21:38:25 +0200

    v

--- app/Http/Requests/StoreImage.php
+++ app/Http/Requests/StoreImage.php
@@ -22,7 +22,7 @@ class StoreImage extends FormRequest
     public function rules(): array
     {
         return [
-            'image' => 'required|image|mimes:jpeg,jpg,png,gif,svg|max:2048'
+            'image.*' => 'required|image|mimes:jpeg,jpg,png,gif,svg|max:2048'
 
         ];
     }
