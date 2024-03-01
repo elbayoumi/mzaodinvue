34ba9d14688d23d39706255b9bcca2f8f195dfd9
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-03-01 15:38:09 +0200

    home

--- app/Http/Controllers/WebSite/HomeController.php
+++ app/Http/Controllers/WebSite/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\WebSite;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 use Illuminate\Http\Request;
@@ -13,14 +14,44 @@ class HomeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
     // dd('dddd');
+    $products = (new Product)->newQuery();
 
-        return Inertia::render('Web/Home', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-        ]);
+    if (request()->has('search')) {
+        $searchTerm = '%' . request()->input('search') . '%';
+
+        $products->where(function ($query) use ($searchTerm) {
+            $query->where('name_arabic', 'LIKE', $searchTerm)
+                ->orWhere('name_english', 'LIKE', $searchTerm)
+                ->orWhere('sku', 'LIKE', $searchTerm)
+                // Add more conditions for other columns as needed
+                ->orWhere('product_details', 'LIKE', $searchTerm);
+        });
+    }
+
+    if (request()->query('sort')) {
+        $attribute = request()->query('sort');
+        $sort_order = 'ASC';
+        if (strncmp($attribute, '-', 1) === 0) {
+            $sort_order = 'DESC';
+            $attribute = substr($attribute, 1);
+        }
+        $products->orderBy($attribute, $sort_order);
+    } else {
+        $products->latest();
+    }
+
+    $products = $products->paginate(5)->onEachSide(2)->appends(request()->query());
+
+    return Inertia::render('Web/Home', [
+        'products' => $products,
+        'filters' => request()->all('search'),
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+
     }
 
     /**
