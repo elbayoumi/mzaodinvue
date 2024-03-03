7f67533e511d63a1102262891cd6209d0b5ea1c7
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-03-03 21:25:23 +0200

    v

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useStyleStore } from '@/Stores/style.js'
+
+const styleStore = useStyleStore()
+</script>
+
+<template>
+  <div>
+        <Navbar :canLogin="canLogin" />
+      <slot />
+</div>
+</template>
