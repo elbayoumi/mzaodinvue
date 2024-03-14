0666387050c373dac4882a9d3494b2f32aedf6f5
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-03-14 18:48:38 +0200

    v

--- resources/js/Pages/Web/Settings.vue
+++ resources/js/Pages/Web/Settings.vue
@@ -1,16 +1,18 @@
 <script setup>
-// import { Head, Link } from '@inertiajs/vue3';
 import Checkbox from '@/Components/Checkbox.vue';
 // import GuestLayout from '@/Layouts/GuestLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { Head, Link, useForm ,usePage , router} from '@inertiajs/vue3';
+import { reactive, computed } from 'vue'
+
 import WebSite from '@/Layouts/WebSite.vue'
 import Navbar from '@/Components/Landing/Navbar.vue';
 import Layout from "@/Layouts/Layout.vue"
-
+const user = computed(() => usePage().props.auth.user)
+console.log('mmmmm,',user)
 // const props = defineProps({
 //     products: {
 //         type: Object,
@@ -85,14 +87,14 @@ const submit = () => {
                             </div>
                             <div class="md:w-1/2 lg:w-1/3 m-auto rounded">
                                 <figure>
-                                    <img src="http://127.0.0.1:8000/website/download.jpeg"
+                                    <img :src="user.image_path"
                                         class="rounded-full w-full h-48 object-cover" alt="Card Image">
                                 </figure>
                             </div>
 
                             <div class="card-body p-4">
                                 <div class="card-h mb-3 flex justify-center">
-                                    <h5 class="card-title text-success">جوال ايفون 14 برو مكس</h5>
+                                    <h5 class="card-title text-success">{{ user.name }}</h5>
                                 </div>
                                 <div class="card-b mb-3 flex justify-end">
                                     <input type="text" class="card-input rounded">
